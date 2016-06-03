3320a409a1ec38667608734803386fc366f43bb2
FaArIsH <faarishhassan@gmail.com>
2016-06-03 08:13:22 +0500

    Update lt02ltexx-common.mk

--- lt02ltexx-common.mk
+++ lt02ltexx-common.mk
@@ -42,10 +42,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x
 
 # Media Profile
 PRODUCT_COPY_FILES += \
